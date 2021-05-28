6e934b9258ceafa5dd9cd143569eb96ed25fd260
imnpc <qilidi@gmail.com>
2021-05-28 15:46:23 +0800

    修改用户API

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -17,16 +17,6 @@ use Spatie\QueryBuilder\QueryBuilder;
 class UserController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      *
      * @param \App\Models\User $user
