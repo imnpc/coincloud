17adffa55714b60cbdb87bff494f91250a35b5dd
imnpc <qilidi@gmail.com>
2021-05-28 15:20:09 +0800

    产品详情API-增加FAQ

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Resources\ProductResource;
+use App\Models\Article;
 use App\Models\Product;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Spatie\QueryBuilder\QueryBuilder;
@@ -20,7 +21,7 @@ class ProductController extends Controller
         AnonymousResourceCollection::wrap('list');// 资源列表默认返回 data 更换为 list
 
         $list = QueryBuilder::for(Product::class)
-            ->orderBy('created_at','desc')
+            ->orderBy('created_at', 'desc')
             ->where('status', '=', 0)
             //->select('id', 'name','price','price_usdt','price_fil','content','desc','thumb')
             ->paginate();
@@ -32,11 +33,14 @@ class ProductController extends Controller
      * 产品详情
      * Display the specified resource.
      *
-     * @param  \App\Models\Product  $product
+     * @param \App\Models\Product $product
      * @return \Illuminate\Http\Response
      */
     public function show(Product $product)
     {
+        $product['buy_faq'] = Article::where('article_category_id', 5)
+            ->where('status', 1)
+            ->get();
         return new ProductResource($product);
     }
 }
--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -27,44 +27,12 @@ class UserController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      *
      * @param \App\Models\User $user
      * @return \Illuminate\Http\Response
      */
     public function show(User $user)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param \App\Models\User $user
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(User $user)
     {
         //
     }
