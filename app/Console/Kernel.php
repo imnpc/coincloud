d662c17b7bf4c57aee8e72377af142a821b5629b
imnpc <qilidi@gmail.com>
2021-06-28 14:18:00 +0800

    计划任务调整

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,9 +30,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->job(new AutoCreateDayBonus)->dailyAt('9:30'); // 每天自动创建分红记录 0:01
-        $schedule->job(new AutoProductBonus)->dailyAt('9:32'); // 产品自动分红 0:05
-        $schedule->job(new AutoProductWeeklyReport)->weekly()->saturdays()->at('10:15'); // 产品每周自动统计报告 每周一 凌晨 0 点 30 执行
+//        $schedule->job(new AutoCreateDayBonus)->dailyAt('9:30'); // 每天自动创建分红记录 0:01
+//        $schedule->job(new AutoProductBonus)->dailyAt('9:32'); // 产品自动分红 0:05
+//        $schedule->job(new AutoProductWeeklyReport)->weekly()->saturdays()->at('10:15'); // 产品每周自动统计报告 每周一 凌晨 0 点 30 执行
         // 服务器
         $schedule->job(new AutoCreateDayBonus)->dailyAt('0:01'); // 每天自动创建分红记录 0:01
         $schedule->job(new ChangeOrderWaitStatus)->dailyAt('0:30'); // 更改订单等待状态 0:30
